16805f7804d30432cf27d48d68b208052bdf699b
PsanchezFR <p.sanchez.fr@gmail.com>
2023-11-28 22:06:19 +0100

    [MOVABLE OBJECT]  switch to draggable logic

--- src/components/MovableObject/MovableObject.jsx
+++ src/components/MovableObject/MovableObject.jsx
@@ -1,41 +1,29 @@
+// REACT
 import { useState } from "react";
+// CSS
 import "./MovableObject.css";
+// DEPENDENCIES
+import Draggable from "react-draggable";
 
+/**
+ *
+ */
 const MovableObject = (props) => {
-  const { color = "red" } = props;
-
-  const DEFAULT_HEIGHT_VALUE = 50;
-  const DEFAULT_WIDTH_VALUE = 50;
-
-  const [height, setHeight] = useState(DEFAULT_HEIGHT_VALUE);
-  const [width, setWidth] = useState(DEFAULT_WIDTH_VALUE);
-  const [coordinates, setCoordinates] = useState([
-    screen.width / 2 - DEFAULT_WIDTH_VALUE,
-    screen.height / 2 - DEFAULT_HEIGHT_VALUE,
-  ]);
-
-  // EVENT HANDLERS
-  const onDragEnd = (event) => {
-    console.log(event.clientX, event.clientY);
-    setCoordinates([event.clientX, event.clientY]);
-  };
-
-  // TOOL FUNCTIONS
+  const { color = "red", height = 50, width = 50 } = props;
 
   return (
-    <div
-      className="movableObject"
-      draggable="true"
-      onDragEnd={onDragEnd}
-      style={{
-        position: "absolute",
-        left: coordinates[0],
-        top: coordinates[1],
-        height,
-        width,
-        backgroundColor: color,
-      }}
-    ></div>
+    <Draggable>
+      <div
+        className="movableObject"
+        onClick={() => {}}
+        style={{
+          position: "absolute",
+          height,
+          width,
+          backgroundColor: color,
+        }}
+      ></div>
+    </Draggable>
   );
 };
 
